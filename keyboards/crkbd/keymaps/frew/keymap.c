9f524aff3abc6c754bbbb36a8e469c3c8319b12d
Arthur Axel 'fREW' Schmidt <git@frew.co>
2021-05-18 21:36:57 -0700

    frew: add Esc key

--- keyboards/crkbd/keymaps/frew/keymap.c
+++ keyboards/crkbd/keymaps/frew/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BSPC, KC_LGUI, KC_LCTL,    KC_PGDN,  KC_ENT, KC_SPC
+                                          KC_BSPC, KC_LGUI, LCTL_T(KC_ESC),    KC_PGDN,  KC_ENT, KC_SPC
                                       //`--------------------------'  `--------------------------'
 
   ),
