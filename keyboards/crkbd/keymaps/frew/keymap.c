189348d9a3cbdeef98d5285f5c0d5f9a17d57aa9
Arthur Axel 'fREW' Schmidt <git@frew.co>
2021-05-19 22:21:53 -0700

    frew: remove accidentally committed comments

--- keyboards/crkbd/keymaps/frew/keymap.c
+++ keyboards/crkbd/keymaps/frew/keymap.c
@@ -16,9 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// TODO: =
-// TODO: Alt
-
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
